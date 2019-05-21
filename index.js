7df6c8308aa962fd80e03e66d27ec3fdc27c286d
John Meade <john.meade.webdev@gmail.com>
2019-05-21 15:37:29 -0600

    add functional search and styling

--- index.js
+++ index.js
@@ -1,23 +1,183 @@
 let meteorInfo;
-let table = document.querySelector('#table');
+let table = document.querySelector("#table");
 let tableCurrentIndex = 0;
+let searchAPIAddress = "https://data.nasa.gov/resource/gh4g-9sfh.json?name=";
 
-apiCall();
+/* function called within any api calls that checks for errors */
 
-function apiCall() {
-    fetch('https://data.nasa.gov/resource/gh4g-9sfh.json')
-        .then(checkStatus)
-        .then(res => res.json())
-        .then((data) => {meteorInfo = data; populateData(meteorInfo)})
-        .catch(err => console.log(err));
+function checkStatus(res) {
+  if (res.ok) {
+    return Promise.resolve(res);
+  } else {
+    return Promise.reject(new Error(res.statusText));
+  }
 }
 
-function checkStatus(res){
-    if (res.ok) {
-        return Promise.resolve(res);
-    } else {
-        return Promise.reject(new Error(res.statusText));
+/* General API Call which is completed on page Load and populated immediately */
+
+function apiCall() {
+  fetch("https://data.nasa.gov/resource/gh4g-9sfh.json")
+    .then(checkStatus)
+    .then(res => res.json())
+    .then(data => {
+      meteorInfo = data;
+      populateData(meteorInfo);
+    })
+    .catch(err => console.log(err));
+}
+
+/* Name Search API Call, completed upon 'enter' of Submit input on search element */
+
+function searchAPICall(name) {
+  fetch(`${searchAPIAddress}${name}`)
+    .then(checkStatus)
+    .then(res => res.json())
+    .then(data => {
+      meteorSearchInfo = data;
+      if(meteorSearchInfo.length > 0){
+        populateData(meteorSearchInfo, 1);
+      } else {
+          table.innerText = 'No Results Found';
+      }
+    })
+    .catch(err => console.log(err));
+}
+
+/* Table Creation Functions */
+
+function createTD(dataPoint) {
+  let td = document.createElement("td");
+  td.innerText = dataPoint;
+  return td;
+}
+
+function createTH(dataPoint) {
+  let th = document.createElement("th");
+  th.innerText = dataPoint;
+  return th;
+}
+
+function tableDataCollection(
+  fall,
+  id,
+  mass,
+  name,
+  recclass,
+  reclat,
+  reclong,
+  year
+) {
+  let tdArr = [];
+  for (let arg in arguments) {
+    if (arguments[arg] == undefined) {
+      arguments[arg] = "UA";
     }
+    tdArr.push(createTD(arguments[arg]));
+  }
+  return tdArr;
+}
+
+function createTableHeader() {
+  let headerRow = document.createElement("tr");
+  let headerTextArr = [
+    "ID",
+    "Name",
+    "Fall",
+    "Mass(g)",
+    "Class",
+    "Latitude",
+    "Longitude",
+    "Year"
+  ];
+  for (let each in headerTextArr) {
+    headerRow.append(createTH(headerTextArr[each]));
+  }
+  table.append(headerRow);
+}
+
+function createRow(meteor) {
+  if (meteor.year) {
+    meteor.year = meteor.year.slice(0, 4);
+  }
+
+  let tr = document.createElement("tr");
+  let td = tableDataCollection(
+    meteor.id,
+    meteor.name,
+    meteor.fall,
+    meteor.mass,
+    meteor.recclass,
+    meteor.reclat,
+    meteor.reclong,
+    meteor.year
+  );
+
+  for (let each in td) {
+    tr.append(td[each]);
+  }
+
+  table.appendChild(tr);
+}
+
+function populateData(meteorInfo, returnRowNumber = 20) {
+  createTableHeader();
+  console.log(`Row Number is: ${returnRowNumber}`);
+  for (let i = 0; i < returnRowNumber; i++) {
+    if (returnRowNumber == 20) {
+      tableCurrentIndex++;
+    } else {
+      tableCurrentIndex = 0;
+    }
+    createRow(meteorInfo[i]);
+  }
+}
+
+function clearData() {
+  while (table.hasChildNodes()) {
+    table.removeChild(table.firstChild);
+  }
+}
+
+function forwardPage() {
+  clearData();
+  createTableHeader();
+  let target = tableCurrentIndex + 20;
+//   console.log(`Beginning Index is = ${tableCurrentIndex}\nTarget = ${target}`);
+  for (let i = tableCurrentIndex; i < target && i < meteorInfo.length; i++) {
+    tableCurrentIndex++;
+    createRow(meteorInfo[i]);
+  }
+}
+
+function backPage() {
+  let beginningIndex = tableCurrentIndex - 20;
+  let target = tableCurrentIndex - 40;
+  if (beginningIndex > 0) {
+    clearData();
+    createTableHeader();
+    // console.log(`BeginningIndex = ${beginningIndex}\nTarget = ${target}`);
+    for (let i = target; i < beginningIndex && i >= 0; i++) {
+      tableCurrentIndex--;
+      createRow(meteorInfo[i]);
+    }
+  }
+}
+
+/* Executes the first API call for page load */
+apiCall();
+
+/* Search Function */
+function search() {
+  let searchText = document.querySelector("#searchText").value.toLowerCase();
+  let cappedSearchText =
+    searchText.slice(0, 1).toUpperCase() + searchText.slice(1);
+//   console.log(`Searching for ${cappedSearchText}`);
+  clearData();
+  if (cappedSearchText == "") {
+    populateData(meteorInfo);
+  } else {
+    searchAPICall(cappedSearchText);
+  }
 }
 
 // meteorInfo = [{
@@ -52,85 +212,3 @@ function checkStatus(res){
 //     }
 // ]
 
-function createTD(dataPoint){
-    let td = document.createElement('td');
-    td.innerText = dataPoint;
-    return td;
-}
-
-function createTH(dataPoint){
-    let th = document.createElement('th');
-    th.innerText = dataPoint;
-    return th;
-}
-
-function tableDataCollection(fall,id,mass,name,recclass,reclat,reclong,year){
-    let tdArr = [];
-    for (let arg in arguments) {
-        if(arguments[arg] == undefined){
-            arguments[arg] = 'UA';
-        }
-        tdArr.push(createTD(arguments[arg]));
-    }
-    return tdArr;
-}
-
-function createTableHeader(){
-    let headerRow = document.createElement('tr');
-    let headerTextArr = ['ID', 'Name', 'Fall', 'Mass(g)', 'Class', 'Latitude', 'Longitude', 'Year'];
-    for(let each in headerTextArr){
-        headerRow.append(createTH(headerTextArr[each]));
-    }
-    table.append(headerRow);
-}
-
-function createRow(meteor){
-    if(meteor.year){
-        meteor.year = meteor.year.slice(0,4);
-    }
-
-    let tr = document.createElement('tr');
-    let td = tableDataCollection(meteor.id, meteor.name, meteor.fall,meteor.mass, meteor.recclass, meteor.reclat, meteor.reclong, meteor.year);
-    
-    for(let each in td){
-        tr.append(td[each]);
-    }
-
-    table.appendChild(tr);
-}
-
-function populateData(){
-    createTableHeader();
-    for(let i = 0; i < 20; i++){
-        tableCurrentIndex++;
-        createRow(meteorInfo[i]);
-    }
-}
-
-function clearData(){
-    while(table.hasChildNodes()) {
-        table.removeChild(table.firstChild);
-    }
-}
-
-function forwardPage(){
-    clearData();
-    createTableHeader();
-    let target = tableCurrentIndex + 20;
-    for(let i = tableCurrentIndex; i < target; i++){
-        tableCurrentIndex++;
-        createRow(meteorInfo[i]);
-    }
-}
-
-function backPage(){
-    clearData();
-    createTableHeader();
-    let beginningIndex = tableCurrentIndex;
-    let target = tableCurrentIndex - 20;
-    // console.log(`BeginningIndex = ${beginningIndex}\nTarget = ${target}`);
-    for(let i = target; i < beginningIndex ; i++){
-        tableCurrentIndex--;
-        createRow(meteorInfo[i]);
-    }
-}
